1ed148e38803e69a0c102539784a2bfd4d6cf822
Adam Sinning <asinning25@gmail.com>
2023-07-26 12:03:03 -0600

    added product objects

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
 
 export const Shop = () => {
   return (
@@ -7,7 +9,9 @@ export const Shop = () => {
             <h1>Adam's Shop</h1>
         </div>
         <div className="products">
-
+            {PRODUCTS.map((product) => (
+                <Product data={product} />
+            ))}
         </div>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,102 @@
+import product1 from "./assets/charvel.jpeg";
+import product2 from "./assets/danelectro.jpeg";
+import product3 from "./assets/epiphone.jpeg";
+import product4 from "./assets/flying.jpeg";
+import product5 from "./assets/framus.jpeg";
+import product6 from "./assets/gretsch.jpeg";
+import product7 from "./assets/ibanez.jpeg";
+import product8 from "./assets/jackson.jpeg";
+import product9 from "./assets/les_paul.jpeg";
+import product10 from "./assets/prs.jpeg";
+import product11 from "./assets/rickenbacker.jpeg";
+import product12 from "./assets/sg.jpeg";
+import product13 from "./assets/strat.jpeg";
+import product14 from "./assets/telecaster.jpeg";
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Charvel",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Danelectro",
+        price: 999.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Epiphone",
+        price: 999.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Flying",
+        price: 999.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Framus",
+        price: 999.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Gretsch",
+        price: 999.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Ibanez",
+        price: 999.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Jackson",
+        price: 999.0,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "Les Paul",
+        price: 999.0,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "PRS",
+        price: 999.0,
+        productImage: product10,
+    },
+    {
+        id: 11,
+        productName: "Rickenbacker",
+        price: 999.0,
+        productImage: product11,
+    },
+    {
+        id: 12,
+        productName: "SG",
+        price: 999.0,
+        productImage: product12,
+    },
+    {
+        id: 13,
+        productName: "Strat",
+        price: 999.0,
+        productImage: product13,
+    },
+    {
+        id: 14,
+        productName: "Telecaster",
+        price: 999.0,
+        productImage: product14,
+    }
+]
