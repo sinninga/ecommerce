073d0ba0b6a440cb80f9dfe4841259f8014db0f3
Adam Sinning <asinning25@gmail.com>
2023-07-26 15:50:46 -0600

    added checkout page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
+import "./cart.css"
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
-  return <div>cart</div>;
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
+  
+  return ( 
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />
+          }
+        })}
+      </div>
+      {totalAmount > 0 ? (
+      <div className="checkout">
+        <p>
+          Subtotal: ${totalAmount}
+        </p>
+        <button onClick={() => navigate("/")}> Continue Shopping</button>
+        <button> Checkout</button>
+      </div>
+  ) : (
+    <div className="checkout">
+    <h1> Your Cart is Empty</h1>
+    <button onClick={() => navigate("/")}> Continue Shopping</button>
+    </div>
+  )}
+    </div>
+  );
 };
