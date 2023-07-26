e501072d5ff94941e2b94d238f3ad2fe0fa4be62
Adam Sinning <asinning25@gmail.com>
2023-07-26 11:32:24 -0600

    initial commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        <div className="shopTitle">
+            <h1>Adam's Shop</h1>
+        </div>
+        <div className="products">
+
+        </div>
+    </div>
+  );
+};
