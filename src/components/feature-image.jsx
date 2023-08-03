0da2f99220a8a725205f13bacd835e1e3fa1a751
Adam Sinning <asinning25@gmail.com>
2023-08-03 16:27:57 -0600

    comon

--- src/components/feature-image.jsx
+++ src/components/feature-image.jsx
@@ -6,7 +6,7 @@ import guitarsImage from "../assets/guitars.jpg"; // Import the image
 export const FeatureImage = () => {
     return (
         <div className="img-wrapper">
-            <a href="/ecommerce/#adam-shop" className="guitars-button">Browse Vintage Guitars</a>
+            <a href="/#adam-shop" className="guitars-button">Browse Vintage Guitars</a>
             <img className="feature-img" src={guitarsImage} alt="img" />
         </div>
   )}
