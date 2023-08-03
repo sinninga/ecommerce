36cc9ca5e7af2c5c6a4b642a5e8c0adfa7bf911d
Adam Sinning <asinning25@gmail.com>
2023-08-02 20:32:03 -0600

    router basename changed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-      <Router>
+      <Router basename="/ecommerce">
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
