0618c388615551415976c70bee03672429c0763a
Adam Sinning <asinning25@gmail.com>
2023-08-03 15:48:00 -0600

    change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-      <Router basename="/ecommerce">
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
