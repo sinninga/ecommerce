6b42dde8f2018cdba97492876a9aeffb7c1da473
Adam Sinning <asinning25@gmail.com>
2023-08-03 19:00:36 -0600

    asdfasf

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/ecommerce" element={<Shop />} />
-          <Route path="/ecommerce/cart" element={<Cart />}/>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />}/>
         </Routes>
       </Router> 
       </ShopContextProvider>
